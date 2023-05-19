0021232eefc9f69999e23e75cd923f659925399d
Erik <e.ferrari@arkemis.it>
2023-05-19 18:20:33 +0200

    chore(CI): change action

--- mix.exs
+++ mix.exs
@@ -42,9 +42,6 @@ defmodule Arke.MixProject do
       {:google_api_storage, "~> 0.34.0"},
       {:goth, "~> 1.3.0"},
       {:httpoison, "~> 2.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true}
     ]
   end
 
