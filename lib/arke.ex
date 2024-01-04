da51c4d711dd25cefd0424624e83a35a7c8cba19
Erik <e.ferrari@arkemis.it>
2024-01-04 12:19:46 +0100

    fix: group manager starting

--- lib/arke.ex
+++ lib/arke.ex
@@ -22,24 +22,6 @@ defmodule Arke do
 
   def init(), do: :ok
 
-
-  defp get_arke_modules() do
-    Enum.reduce(:application.loaded_applications(), [], fn {app, _, _}, arke_list ->
-      {:ok, modules} = :application.get_key(app, :modules)
-
-      module_arke_list =
-        Enum.reduce(modules, [], fn mod, mod_arke_list ->
-          is_arke =
-            Code.ensure_loaded?(mod) and :erlang.function_exported(mod, :arke_from_attr, 0) and
-              mod.arke_from_attr != nil and mod.arke_from_attr.remote == true
-
-          mod_arke_list = check_arke_module(mod, mod_arke_list, is_arke)
-        end)
-
-      arke_list ++ module_arke_list
-    end)
-  end
-
   defp get_module(data,type) do
     # get all the arke modules which has the arke macro defined
     # find the right module for the given data and return it
@@ -70,19 +52,6 @@ defmodule Arke do
   defp get_module_fn("arke"), do: :arke_from_attr
   defp get_module_fn("group"), do: :group_from_attr
 
-  defp check_arke_module(mod, arke_list, true) do
-    %{id: id, data: data, metadata: metadata} = mod.arke_from_attr
-    unit = Unit.new(id, data, :arke, nil, metadata, nil, nil, mod)
-
-    ArkeManager.create(unit, :arke_system)
-
-    Enum.map(mod.groups_from_attr, fn %{id: parent_id, metadata: link_metadata} ->
-      GroupManager.add_link(parent_id, :arke_system, :arke_list, id, link_metadata)
-    end)
-
-    [mod | arke_list]
-  end
-
   defp check_arke_module(_, arke_list, false), do: arke_list
 
   def handle_manager(_data,_project,_arke_id,_error\\[])
@@ -104,13 +73,14 @@ defmodule Arke do
   end
 
   def handle_manager([data | t],project,:group,error)do
-    #todo: check if in arke_list we need also metadata besides the id
-    loaded_list = Enum.reduce(Map.get(data,:arke_list,[]),[], fn id,acc ->
-      case Arke.Boundary.ArkeManager.get(id, project) do
-        {:error, _msg} ->
-          [%{context: :arke_list_group , message: "no manager has been found for: `#{id}` in `#{project}`"} | error]
-        arke ->
-          [arke | acc]
+    #todo: check if in arke_list we need also metadata besides the id and if we have to merge the link_data.metadata with the arke.metadat
+    loaded_list = Enum.reduce(Map.get(data,:arke_list,[]),[], fn arke,acc ->
+      with %{id: id, metadata: _metadata}=link_data <- parse_group_member(arke),
+           %Unit{}= arke <- ArkeManager.get(id, project) do
+          [link_data | acc]
+        else
+          _ ->
+            [%{context: :arke_list_group , message: "invalid arke in arke_list of: `#{data.id}` in `#{project}`"} | error]
       end
 
     end)
@@ -118,17 +88,17 @@ defmodule Arke do
                 |> Map.put(:arke_list,loaded_list)
     module = get_module(final_data,"group")
     updated_error = start_manager(final_data,"group",project,GroupManager,module)
-    if length(updated_error) == 0 do
-      Enum.each(final_data.arke_list, fn arke -> link_arke_group(final_data.id,project,arke) end)
-
-    end
     handle_manager(t,project, :group,updated_error ++ error)
   end
 
-  defp link_arke_group(group_id,project, arke) when is_binary(arke), do: link_arke_group(group_id,project,%{id: arke, metadata: %{}})
-  defp link_arke_group(group_id,project, arke) do
-    GroupManager.add_link(group_id, project, :arke_list, arke.id, arke.metadata)
-  end
+
+  defp parse_group_member(member) when is_binary(member), do: %{id: String.to_atom(member), metadata: %{}}
+  defp parse_group_member(member) when is_map(member) do
+    case Map.get(member,:id) do
+    nil -> {:error,"arke id not found"}
+    id -> %{id: id, metadata: Map.get(member, :metadata, %{})}
+    end
+ end
 
   def handle_manager([],_project,_arke_id,error),do: error
   defp start_manager(_data,_type,_project, _manager, _module,_error\\[])
@@ -158,12 +128,24 @@ defmodule Arke do
   end
   defp parse_arke_parameter(data,project) do
 
-    Map.get(data,:parameters) |> Enum.reduce([], fn param,acc ->
+    base_parameters(Map.get(data,:parameters,[])) |> Enum.reduce([], fn param,acc ->
       # todo: fare controllo per cui se esce tbd (sarà poi nil) scrivere sul file che la chiave  id manca
-      converted = Map.update(param,:id, "tbd", &String.to_atom(&1))
+      # controllare anche che il parameter manager torni qualcosa che esiste
+      converted = Map.update(param,:id, "tbd", &String.to_atom(to_string(&1)))
       id = converted[:id]
-      arke = ParameterManager.get(id,project)
-      [ Map.put(converted,:arke, arke.arke_id) | acc]
+      case ParameterManager.get(id,project) do
+        %Unit{} = arke ->  [ Map.put(converted,:arke, arke.arke_id) | acc]
+        _ -> acc
+      end
     end)
+  end
+
+  defp base_parameters(arke_parameters) do
+    arke_parameters ++ [
+      %{id: "id", metadata: %{required: true, persistence: "table_column"}},
+      %{id: "arke_id", metadata: %{required: false, persistence: "table_column"}},
+      %{id: "metadata", metadata: %{required: false, persistence: "table_column"}},
+      %{id: "inserted_at", metadata: %{required: false, persistence: "table_column"}},
+      %{id: "updated_at", metadata: %{required: false, persistence: "table_column"}}]
   end
 end
