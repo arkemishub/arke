f3033d6ae96ed12c603ebf8dfde3077be7adce2a
Erik <e.ferrari@arkemis.it>
2024-02-20 13:18:16 +0100

    fix: link parameters

--- mix.exs
+++ mix.exs
@@ -60,7 +60,9 @@ defmodule Arke.MixProject do
       {:goth, "~> 1.3.0"},
       {:httpoison, "~> 2.0"},
       {:calendar, "~> 1.0.0"},
-      {:xlsxir, "~> 1.6"}
+      {:xlsxir, "~> 1.6"},
+      {:libcluster, "~> 3.3"},
+      {:node_registry, "~> 0.1.1"},
     ]
   end
 
