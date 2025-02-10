67c6cf6761a030f70673af585fe2e60ecf78bdca
Ilyich Vismara <81801314+ilyichv@users.noreply.github.com>
2025-02-10 18:04:01 +0100

    wip: bulk links (#118)

--- lib/arke/link_manager.ex
+++ lib/arke/link_manager.ex
@@ -14,48 +14,29 @@
 
 defmodule Arke.LinkManager do
   @moduledoc false
-  @record_fields [:id, :data, :metadata, :inserted_at, :updated_at]
 
   alias Arke.Boundary.ArkeManager
   alias Arke.Utils.ErrorGenerator, as: Error
   alias Arke.QueryManager
   alias Arke.Core.Unit
   alias Arke.Core.Query
+  alias Arke.Core.Link
 
-  defmodule LinkData do
-    @enforce_keys [:parent_id, :child_id, :type]
-    defstruct [:parent_id, :child_id, :type, metadata: %{}]
-
-    def to_map(link_data) do
-      Map.take(link_data, [:parent_id, :child_id, :type, :metadata])
-    end
-  end
-
-  def add_node(project, %Unit{} = parent, %Unit{} = child, type \\ "link", metadata \\ %{}) do
+  def add_node(project, parent, child, type \\ "link", metadata \\ %{}) do
     arke_link = ArkeManager.get(:arke_link, project)
+    link = %{"parent" => parent, "child" => child, "type" => type, "metadata" => metadata}
 
-    case check_link(project, parent, child, arke_link) do
-      {_, nil} ->
-        QueryManager.create(project, arke_link,
-          parent_id: Atom.to_string(parent.id),
-          child_id: Atom.to_string(child.id),
-          type: type,
-          metadata: metadata
-        )
-
-      {:ok, _} ->
-        Error.create(:link, "link already exists")
-    end
-  end
-
-  def add_node(project, parent, child, type, metadata)
-      when is_binary(parent) and is_binary(child) do
-    with %Unit{} = unit_parent <- QueryManager.get_by(id: parent, project: project),
-         %Unit{} = unit_child <- QueryManager.get_by(id: child, project: project) do
-      add_node(project, unit_parent, unit_child, type, metadata)
-    else
-      _ -> Error.create(:link, "parent: `#{parent}` or child: `#{child}` not found")
-    end
+    with {valid, _} <-
+           validate_links(project, arke_link, [link]),
+         {[link], _} <- validate_existing(project, arke_link, valid, []),
+         do:
+           QueryManager.create(project, arke_link,
+             parent_id: link.parent_id,
+             child_id: link.child_id,
+             type: link.type,
+             metadata: link.metadata
+           ),
+         else: (_ -> Error.create(:link, "link already exists"))
   end
 
   def add_node(_project, _parent, _child, _type, _metadata),
@@ -64,7 +45,7 @@ defmodule Arke.LinkManager do
   def add_node_bulk(project, link_list) do
     arke_link = ArkeManager.get(:arke_link, project)
 
-    with {valid, errors} <- validate_units(project, arke_link, link_list),
+    with {valid, errors} <- validate_links(project, arke_link, link_list),
          {valid, errors} <- validate_existing(project, arke_link, valid, errors),
          {:ok, inserted_count, valid, insert_errors} <-
            QueryManager.create_bulk(project, arke_link, prepare_link_data(valid), []) do
@@ -74,41 +55,40 @@ defmodule Arke.LinkManager do
     end
   end
 
-  def update_node(project, %Unit{} = parent, %Unit{} = child, type, metadata) do
+  def update_node(project, parent, child, type, metadata) do
     arke_link = ArkeManager.get(:arke_link, :arke_system)
+    link = %{"parent" => parent, "child" => child, "type" => type, "metadata" => metadata}
 
-    case check_link(project, parent, child, arke_link) do
-      {:error, _} -> Error.create(:link, "link not found")
-      {:ok, link} -> QueryManager.update(link, metadata: metadata, type: type)
+    with {[valid_link], _} <- validate_links(project, arke_link, [link]),
+         [existing_link] <- existing_links(project, arke_link, [valid_link]) do
+      QueryManager.update(existing_link, metadata: metadata)
+    else
+      _ -> Error.create(:link, "link not found")
     end
-  end
-
-  def update_node(project, parent, child, type, metadata)
-      when is_binary(parent) and is_binary(child) do
-    unit_parent = QueryManager.get_by(id: parent, project: project)
-    unit_child = QueryManager.get_by(id: child, project: project)
-
-    update_node(project, unit_parent, unit_child, type, metadata)
   end
 
   def update_node(_project, _parent, _child, _type, _metadata),
     do: Error.create(:link, "invalid parameters")
 
-  def delete_node(project, %Unit{} = parent, %Unit{} = child, type, metadata \\ %{}) do
-    arke_link = ArkeManager.get(:arke_link, :arke_system)
+  def update_node_bulk(project, link_list) do
+    arke_link = ArkeManager.get(:arke_link, project)
 
-    case check_link(project, parent, child, arke_link) do
-      {:error, _} -> Error.create(:link, "link not found")
-      {:ok, link} -> QueryManager.delete(project, link)
-    end
+    {valid, errors} = validate_links(project, arke_link, link_list)
+    existing = existing_links(project, arke_link, valid)
+
+    QueryManager.update_bulk(project, arke_link, existing, prepare_link_data(valid))
   end
 
-  def delete_node(project, parent, child, type, metadata)
-      when is_binary(parent) and is_binary(child) do
-    unit_parent = QueryManager.get_by(id: parent, project: project)
-    unit_child = QueryManager.get_by(id: child, project: project)
+  def delete_node(project, parent, child, type, metadata \\ %{}) do
+    arke_link = ArkeManager.get(:arke_link, :arke_system)
+    link = %{"parent" => parent, "child" => child, "type" => type, "metadata" => metadata}
 
-    delete_node(project, unit_parent, unit_child, type, metadata)
+    with {[valid_link], _} <- validate_links(project, arke_link, [link]),
+         [existing_link] <- existing_links(project, arke_link, [valid_link]) do
+      QueryManager.delete(project, existing_link)
+    else
+      _ -> Error.create(:link, "link not found")
+    end
   end
 
   def delete_node(_project, _parent, _child, _type, _metadata),
@@ -116,60 +96,35 @@ defmodule Arke.LinkManager do
 
   def delete_node_bulk(project, link_list) do
     arke_link = ArkeManager.get(:arke_link, :arke_system)
-    {valid, _} = validate_units(project, arke_link, link_list)
+    {valid, _} = validate_links(project, arke_link, link_list)
 
     QueryManager.delete_bulk(project, existing_links(project, arke_link, valid))
   end
 
-  defp check_link(project, parent, child, arke_link) do
-    with %Arke.Core.Unit{} = link <-
-           Arke.QueryManager.query(project: project, arke: arke_link)
-           |> Arke.QueryManager.filter(:parent_id, :eq, Atom.to_string(parent.id), false)
-           |> Arke.QueryManager.filter(:child_id, :eq, Atom.to_string(child.id), false)
-           |> Arke.QueryManager.one(),
-         do: {:ok, link},
-         else: (_ -> {:error, nil})
-  end
+  @spec validate_links(atom(), Arke.t(), list()) :: {list(Link.t()), list(map())}
+  defp validate_links(project, arke_link, [%Link{} | _] = link_list),
+    do: {link_list, []}
 
-  @spec validate_units(atom(), Arke.t(), list()) :: {list(LinkData.t()), list(map())}
-  defp validate_units(project, arke_link, link_list) do
-    ids =
-      Enum.flat_map(link_list, fn link ->
-        case {link["parent_id"], link["child_id"]} do
-          {parent, child} when is_binary(parent) and is_binary(child) ->
-            [parent, child]
-
-          {parent, _} when is_binary(parent) ->
-            [parent]
-
-          {_, child} when is_binary(child) ->
-            [child]
-
-          _ ->
-            []
-        end
+  defp validate_links(project, arke_link, [%Unit{} | _] = unit_list) do
+    links =
+      Enum.map(unit_list, fn unit ->
+        Link.load(project, unit)
       end)
 
-    unit_list = QueryManager.filter_by(id__in: ids, project: project)
-    unit_map = Map.new(unit_list, fn unit -> {Atom.to_string(unit.id), unit} end)
+    {links, []}
+  end
 
+  defp validate_links(project, arke_link, link_list) do
     Enum.reduce(link_list, {[], []}, fn link, {valid, errors} ->
-      parent =
-        case link["parent_id"] do
-          %Unit{} = p -> p
-          id -> Map.get(unit_map, id)
-        end
-
-      child =
-        case link["child_id"] do
-          %Unit{} = c -> c
-          id -> Map.get(unit_map, id)
-        end
-
+      parent = link["parent"]
+      child = link["child"]
       type = Map.get(link, "type", "link")
       metadata = Map.get(link, "metadata", %{})
 
       case {parent, child} do
+        {parent, child} when is_binary(parent) and is_binary(child) ->
+          {[Link.new(parent, child, type, metadata) | valid], errors}
+
         {nil, nil} ->
           {valid, [%{link: link, error: "invalid parent and child"} | errors]}
 
@@ -179,22 +134,17 @@ defmodule Arke.LinkManager do
         {_, nil} ->
           {valid, [%{link: link, error: "invalid child"} | errors]}
 
-        {p, c} ->
-          {[
-             %LinkData{
-               parent_id: Atom.to_string(p.id),
-               child_id: Atom.to_string(c.id),
-               type: type,
-               metadata: metadata
-             }
-             | valid
-           ], errors}
+        _ ->
+          {valid, [%{link: link, error: "invalid parent and child format"} | errors]}
       end
     end)
   end
 
-  @spec existing_links(project :: atom(), arke_link :: Arke.t(), link_list :: list(LinkData.t())) ::
+  @spec existing_links(project :: atom(), arke_link :: Arke.t(), link_list :: list(Link.t())) ::
           list(Arke.Core.Unit.t())
+
+  defp existing_links(_project, _arke_link, []), do: []
+
   defp existing_links(project, arke_link, link_list) do
     parameters = ArkeManager.get_parameters(arke_link)
     parent_id = Enum.find(parameters, fn p -> p.id == :parent_id end)
@@ -218,8 +168,8 @@ defmodule Arke.LinkManager do
     |> Arke.QueryManager.all()
   end
 
-  @spec validate_existing(atom(), Arke.t(), list(LinkData.t()), list()) ::
-          {list(LinkData.t()), list()}
+  @spec validate_existing(atom(), Arke.t(), list(Unit.t()), list()) ::
+          {list(Link.t()), list()}
   defp validate_existing(project, arke_link, link_list, errors) do
     existing =
       existing_links(project, arke_link, link_list)
@@ -229,14 +179,18 @@ defmodule Arke.LinkManager do
 
     Enum.reduce(link_list, {[], errors}, fn link, {valid_acc, errors_acc} ->
       case MapSet.member?(existing, {link.parent_id, link.child_id, link.type}) do
-        true -> {valid_acc, [LinkData.to_map(link) | errors_acc]}
-        false -> {[LinkData.to_map(link) | valid_acc], errors_acc}
+        true -> {valid_acc, [data_to_map(link) | errors_acc]}
+        false -> {[data_to_map(link) | valid_acc], errors_acc}
       end
     end)
   end
 
-  @spec prepare_link_data(list(LinkData.t())) :: list(map())
+  @spec prepare_link_data(list(Link.t())) :: list(map())
   defp prepare_link_data(link_list) do
-    Enum.map(link_list, &LinkData.to_map/1)
+    Enum.map(link_list, &data_to_map/1)
+  end
+
+  defp data_to_map(link) do
+    Map.take(link, [:parent_id, :child_id, :type, :metadata])
   end
 end
